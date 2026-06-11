35d7b3660cbb4b4f994f11df84378955d7ccdfcf
dbt <23521563@gm.uit.edu.vn>
2026-06-11 23:04:39 +0700

    fix to connect with be

--- FE/src/lib/api.ts
+++ FE/src/lib/api.ts
@@ -1,6 +1,6 @@
 // src/lib/api.ts
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8080";
+const BASE_URL = "https://movie-recommendation-uit.onrender.com";
 
 let refreshPromise: Promise<string> | null = null;
 
